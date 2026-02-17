15486147e91da1624c9f76125d63ca2c6f9553bf
maririn100 <108440254+maririn100@users.noreply.github.com>
2026-02-17 14:00:41 +0900

    フォーマット

--- functions/src/controllers/ReportsController.ts
+++ functions/src/controllers/ReportsController.ts
@@ -9,55 +9,54 @@ import type { ReportRecord } from "../types";
 import { storeReport } from "../stores";
 
 interface CreateParams {
-    authorization: string;
-    contentId: string;
-    category: string;
-    description?: string;
+	authorization: string;
+	contentId: string;
+	category: string;
+	description?: string;
 }
 
 export class ReportsController extends BaseController {
-    constructor(app: App) {
-        super(app);
-        this.validators.post = [
-            fw.params.InstantValidator(
-                [
-                    params.headerBearerTokenValidator(),
-                    validators.body("contentId").isString().notEmpty(),
-                    validators.body("category").isString().notEmpty(),
-                    validators.body("description").optional().isString(),
-                ],
-                (context) =>
-                    ({
-                        authorization: context.req.headers.authorization,
-                        contentId: context.req.body.contentId,
-                        category: context.req.body.category,
-                        description: context.req.body.description,
-                    }) as CreateParams,
-            ),
-        ];
+	constructor(app: App) {
+		super(app);
+		this.validators.post = [
+			fw.params.InstantValidator(
+				[
+					params.headerBearerTokenValidator(),
+					validators.body("contentId").isString().notEmpty(),
+					validators.body("category").isString().notEmpty(),
+					validators.body("description").optional().isString(),
+				],
+				(context) =>
+					({
+						authorization: context.req.headers.authorization,
+						contentId: context.req.body.contentId,
+						category: context.req.body.category,
+						description: context.req.body.description,
+					}) as CreateParams,
+			),
+		];
+	}
 
-    }
+	register(basePath: string): Router {
+		const router = super.register(basePath);
 
-    register(basePath: string): Router {
-        const router = super.register(basePath);
+		return router;
+	}
 
-        return router;
-    }
-
-    async post(context: Context) {
-        const p = context.params as CreateParams;
-        const verifyResult = await this.verify(p.authorization);
-        const reportData = {
-            reporterId: verifyResult.uid,
-            contentId: p.contentId,
-            category: p.category,
-            description: p.description,
-        } as Pick<ReportRecord, "reporterId" | "contentId" | "category" | "description">;
-        const reportId = await storeReport(this.app.firestore, {
-            ...reportData,
-        });
-        return {
-            reportId,
-        };
-    }
+	async post(context: Context) {
+		const p = context.params as CreateParams;
+		const verifyResult = await this.verify(p.authorization);
+		const reportData = {
+			reporterId: verifyResult.uid,
+			contentId: p.contentId,
+			category: p.category,
+			description: p.description,
+		} as Pick<ReportRecord, "reporterId" | "contentId" | "category" | "description">;
+		const reportId = await storeReport(this.app.firestore, {
+			...reportData,
+		});
+		return {
+			reportId,
+		};
+	}
 }

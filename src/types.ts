e5fcfe68d82a95b716e9a1b94831088d02b7b4e2
takakushogo <t.shoogo@icloud.com>
2026-09-04 20:28:45 +0900

    テスト

--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,7 @@ export interface ContentRecord {
 	ownerId: string;
 	title: string;
 	description?: string;
-	contentJsonPath: string;
+	contentJsonPath?: string;
 	zipUrl?: string | null;
 	thumbnailUrl?: string;
 	extractedPath?: string | null;
--- src/types/loadAgvw.d.ts
+++
@@ -1,1 +0,0 @@
-declare function require(x: string): any;
